f6f6c8e830ce7144ec22182620eb72c8089f23e2
Sergey Zarochentsev <zsergey26@gmail.com>
2015-11-16 21:53:17 +0200

    add fragment

--- app/src/main/java/com/zsergei/unifiedstateexam/view/activity/BaseActivity.java
+++ app/src/main/java/com/zsergei/unifiedstateexam/view/activity/BaseActivity.java
@@ -1,9 +1,17 @@
 package com.zsergei.unifiedstateexam.view.activity;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Sergei Zarochentsev on 16.11.2015.
  */
 public class BaseActivity extends AppCompatActivity {
+
+    protected void addFragment(int containerViewId, Fragment fragment) {
+        FragmentTransaction fragmentTransaction = this.getFragmentManager().beginTransaction();
+        fragmentTransaction.add(containerViewId, fragment);
+        fragmentTransaction.commit();
+    }
 }
